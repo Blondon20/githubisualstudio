f5e03d9e7222f746ba5ffe3fb2dacd97d6074af1
Blondon20 <gbprogrammeur@gmail.com>
2023-07-12 18:05:17 +0200

    Cretion de l clsse vehicule

---
+++ WindowsFormsAp/WindowsFormsAp/Vehicule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsAp
+{
+    class Vehicule
+    {
+        public string nom;
+        public int sexe;
+        /// <summary>
+        /// mon utilitii
+        /// </summary>
+        /// <param name="_nom"> 2 prm</param>
+        /// <param name="_sexe"></param>
+        public Vehicule(string _nom, int _sexe)
+        {
+            this.nom = _nom;
+            this.sexe = _sexe;
+        }
+    }
+}
